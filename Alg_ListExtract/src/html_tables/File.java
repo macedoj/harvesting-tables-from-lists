a0550847277bf65222083d78fbdfb43194a69811
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-11-05 13:32:13 -0200

    [File] Add new class File.java

---
+++ Alg_ListExtract/src/html_tables/File.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package html_tables;
+
+/**
+ *
+ * @author PC
+ */
+public class File {
+    
+    private String nameFile;
+     private String rowFile;
+
+    /**
+     * @return the nameFile
+     */
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    /**
+     * @param nameFile the nameFile to set
+     */
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+
+    /**
+     * @return the rowFile
+     */
+    public String getRowFile() {
+        return rowFile;
+    }
+
+    /**
+     * @param rowFile the rowFile to set
+     */
+    public void setRowFile(String rowFile) {
+        this.rowFile = rowFile;
+    }
+    
+}
