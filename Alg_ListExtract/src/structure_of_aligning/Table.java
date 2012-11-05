763ef3058f7777b917508af20b5dd2ca10310fec
Juliano Rodovalho <j.rodovalho.m@gmail.com>
2012-11-05 17:36:34 -0200

    [Column and Table] Added two new class

---
+++ Alg_ListExtract/src/structure_of_aligning/Table.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package structure_of_aligning;
+
+/**
+ *
+ * @author PC
+ */
+public class Table {
+    
+}
