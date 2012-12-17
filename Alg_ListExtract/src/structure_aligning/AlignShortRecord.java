552d40a4261bfe2ab4cdfbd354af04ca02f6f331
Juliano <j.rodovalho.m@gmail.com>
2012-12-17 16:54:39 -0200

    [AlignShortRecord] Refactoring

--- Alg_ListExtract/src/structure_aligning/AlignShortRecord.java
+++ Alg_ListExtract/src/structure_aligning/AlignShortRecord.java
@@ -28,42 +28,45 @@ public class AlignShortRecord {
     @SuppressWarnings("CallToThreadDumpStack")
     public void pre_AlignShortRecord(String field, ArrayList<ArrayList<FieldCandidate>> rowsCadidates, int idealNumColumn) {
 
-        int maxRepeticoes = 0;
-        int posicaoDaColuna = 0;
         int numLinhas = rowsCadidates.size();
-        // System.out.println("\nCampo: " + field + "[" + correctColumn.size() + "]");
 
         try {
             for (int j = 0; j < idealNumColumn; j++) {
-
                 int repeticoes = 0;
                 for (int i = 0; i < rowsCadidates.size(); i++) {
 
                     if (rowsCadidates.get(i).size() == idealNumColumn) {
 
                         String fieldCorrectColumn = rowsCadidates.get(i).get(j).getField();
-                        //   System.out.println("Other field: " + fieldCorrectColumn);
 
                         if (field.equals(fieldCorrectColumn)) {
 
                             repeticoes++;
-                            System.out.println(field + " == " + fieldCorrectColumn + " < Num coluna:" + posicaoDaColuna);
+                            System.out.println(field + " == " + fieldCorrectColumn + " < Num coluna:" + j);
                         }
                     }
                 }
 
-                if (repeticoes > maxRepeticoes) {
+                if (repeticoes > 0) {
 
-                    System.out.println("Repetição total: " + repeticoes);
-                    posicaoDaColuna = j;
-                    maxRepeticoes = repeticoes;
+                    String calculo = (String.format("%,.2f", Float.valueOf(repeticoes * 100) / numLinhas));
+
+                    System.out.println(numLinhas + " | " + repeticoes + " \nColuna: " + j + " - Igualidaddes: " + calculo);
                 }
+
+                /*          if (repeticoes > maxRepeticoes) {
+
+                 System.out.println("Repetição total: " + repeticoes);
+                 posicaoDaColuna = j;
+                 maxRepeticoes = repeticoes;
+                 }
+                 }
+
+                 float scoreAtual = (((float) maxRepeticoes) / numLinhas);
+                 System.out.println("Field: " + field);
+                 System.out.println("Maior Score: " + scoreAtual + " - Coluna: " + posicaoDaColuna);
+                 */
             }
-
-            float scoreAtual = (((float) maxRepeticoes) / numLinhas);
-            System.out.println("Field: " + field);
-            System.out.println("Maior Score: " + scoreAtual + " - Coluna: " + posicaoDaColuna);
-
         } catch (Exception error) {
             /**
              * Show the StackTrace error [for debug]
