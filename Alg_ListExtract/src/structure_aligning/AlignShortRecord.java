f4ff07f0a1ce2aa0c3704d2722b7677d5bd6dd95
Juliano <j.rodovalho.m@gmail.com>
2013-04-04 14:26:27 -0300

    [Structure-Aligning] Refactoring the class AlignShortRecord!

--- Alg_ListExtract/src/structure_aligning/AlignShortRecord.java
+++ Alg_ListExtract/src/structure_aligning/AlignShortRecord.java
@@ -7,11 +7,7 @@ package structure_aligning;
 import alg_listextract.FieldCandidate;
 import alg_listextract.FieldExtractor;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import regex_extractor.CurrencyValuesExtractor;
@@ -25,118 +21,122 @@ import regex_extractor.ZipCodeBRExtractor;
 import regex_extractor.ZipCodeEUAExtractor;
 
 /**
+ * <p>
  * This class is responsible for aligning records that contains less columns
- * [with field candidates] than ideal.
+ * <tt>[with field candidates]</tt> than ideal.
+ * </p>
  *
- * @since 06/02/2013 - Last change: 14/02/2013
- * @version 0.1
  * @author Juliano R. Macedo
+ * @version 0.1
+ * @since 06/02/2013 - Last change: 28/03/2013
  * @link https://github.com/JulianoR/Extract-Tables-from-Lists
  *
  */
 public class AlignShortRecord {
 
-    private static AlignShortRecord objAlgnSR;
-    List<FieldExtractor> list;
+    private static AlignShortRecord objAlgnSR = null;
+    private List<FieldExtractor> list;
 
-    public AlignShortRecord() {
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private AlignShortRecord() {
+        list = new ArrayList<>();
     }
 
     /**
-     * Depois da última atualização da lasse, onde o algoritmo de Nedleman foi
+     * <p>
+     * This is Singleton method of class AlignShortRecord!
+     * </p>
+     *
+     * Depois da última atualização da classe, onde o algoritmo de Nedleman foi
      * removido, não é mais necessário esse método singleton.
      *
-     * @see This is Singleton method of class AlignShortRecord!
      * @return the instance of this class
      */
     public static AlignShortRecord getInstance() {
-
         if (objAlgnSR == null) {
             objAlgnSR = new AlignShortRecord();
         }
         return objAlgnSR;
     }
 
-    /**
-     *
-     * @param shortRecord
-     * @param rowsCadidates
-     * @param idealNumColumn
-     */
     public void geraMatrizScors(ArrayList<FieldCandidate> shortRecord,
             ArrayList<ArrayList<FieldCandidate>> rowsCadidates, int idealNumColumn) {
 
-        ArrayList<ArrayList<FieldCandidate>> correctColumns = new ArrayList<>();
+        ArrayList<ArrayList<FieldCandidate>> correctColumns = new ArrayList<ArrayList<FieldCandidate>>();
+        Score score = Score.getInstance();
+        Line line = new Line();
+        Value value = new Value();
+        Table table = new Table();
+        double equalsFieldScore = 0;
+        double scoreBasedExpressions = 0;
 
-        //Carregando a lista apenas com as colunas corretas (=idealNumColumn)
         for (int i = 0; i < rowsCadidates.size(); i++) {
-
             if (rowsCadidates.get(i).size() == idealNumColumn) {
                 correctColumns.add(rowsCadidates.get(i));
             }
         }
 
-        //Dimensão 0, são os valores, na dimensao 1, são as colunas
-        double[][] matrizScors = new double[shortRecord.size()][idealNumColumn];
-        double percentagem = 0;
+        for (int i = 0; i < shortRecord.size(); i++) {
+            boolean mustAdd = false;
 
-        for (int j = 0; j < shortRecord.size(); j++) {
-            for (int i = 0; i < idealNumColumn; i++) {
+            for (int j = 0; j < idealNumColumn; j++) {
 
-                if (correctColumns.get(i).size() == idealNumColumn) {
-
-                    int soma = 0;
-                    int tamColuna = 0;
-                    tamColuna = correctColumns.size(); // Número de Linhas de cada coluna
+                if (correctColumns.get(j).size() == idealNumColumn) {
+                    mustAdd = true;
+                    double occurrencesSum = 0;
+                    int numberLinesCorrectColumns = 0;
+                    numberLinesCorrectColumns = correctColumns.size();
 
                     for (int t = 0; t < correctColumns.size(); t++) {
-
-                        if (correctColumns.get(i).size() == idealNumColumn) {
-                            //Recebe o retor binário depois do cálculo.
-                            soma += funcaoCompara(shortRecord.get(j).getField(), correctColumns.get(t).get(i).getField());
-                            /// No "rowsCadidates.get(t).get(i).getField()" o get(i) gera exceção dentro de linhas 
-                            // que contem menor quantidade de valores. Gerar a tabela com as linhas que contem os numeros de campos validos.
+                        if (correctColumns.get(j).size() == idealNumColumn) {
+                            occurrencesSum += funcaoCompara(shortRecord.get(i).getField(), correctColumns.get(t).get(j).getField());
+                            value.setValor(shortRecord.get(i).getField());
                         }
                     }
+                    equalsFieldScore = computePercentage(occurrencesSum, numberLinesCorrectColumns);
+                    scoreBasedExpressions = scoreBasedExpressions(correctColumns, idealNumColumn, shortRecord.get(i).getField());
+
                     /**
-                     * Olhar o excel para realizar as alterações.
+                     * Verificar a construção da list de scores na classe Line!
                      */
-                    percentagem = computePercentage(soma, tamColuna);
-                    matrizScors[j][i] = percentagem;
+                    score.setScore(equalsFieldScore);
+                    line.addScore(score); //Add o Score que acabou de ser definido.
+                    System.out.println(">" + line.getScores());
+
+                    if (scoreBasedExpressions != Double.MAX_VALUE) {
+                        score.setScore(scoreBasedExpressions);
+                        line.addScore(score);
+                        System.out.println(line.getScores());
+                    } else;
                 }
             }
+            if (mustAdd) {
+                value.addLine(line);
+//                System.out.println(line.getScores());
+//                System.out.println(value.getLines());
+                table.addValue(value);
+//                System.out.println(table.getValuesScores());
+            }
         }
+        //        System.out.println(table.getValuesScores());
+
         System.out.println("------");
-        // printaMatriz(matrizScors);
-        //calcRankingExpress(correctColumns, idealNumColumn);
-//        
-//        System.out.println("Field: "+correctColumns.get(0).get(0).getField());
-//        System.out.println("->> "+ getScoreField(correctColumns.get(0).get(0).getField()));
-//        System.out.println("------");
-//        System.out.println("Field: "+correctColumns.get(2).get(2).getField());
-//        System.out.println("->> "+ getScoreField(correctColumns.get(3).get(4).getField()));
-
-
-        System.out.println("Data: " + getScoreField("21/12/2012"));
-
-
-
-        /**
-         * Aqui vai o resto da constiaç
-         */
+//        printaMatriz(matrizScors);
     }
 
     /**
      *
-     * @param campo
-     * @param campoDaColuna
-     * @return
+     * @param field
+     * @param columnField
+     * @return a porcentagem da comparação
      */
-    private int funcaoCompara(String campo, String campoDaColuna) {
+    private double funcaoCompara(String field, String columnField) {
 
-        int porcentagemComparacao = 0;
+        double porcentagemComparacao = 0;
 
-        if (campo.equals(campoDaColuna)) {
+        if (field.equals(columnField)) {
 
             porcentagemComparacao = 1;
             return porcentagemComparacao;
@@ -146,82 +146,38 @@ public class AlignShortRecord {
     }
 
     /**
+     * <p>
      * Compute the percentage of similarity the field for the others fields.
+     * </p>
      *
-     * @param soma
-     * @param tamColuna
+     * @param occurrencesSum
+     * @param numberLinesCorrectColumns
      * @return the percentage of equals fields.
      */
-    private double computePercentage(int soma, int tamColuna) {
-
-        return ((double) ((soma * 100) / tamColuna));
+    private double computePercentage(double occurrencesSum, int numberLinesCorrectColumns) {
+        return ((double) ((occurrencesSum * 100) / numberLinesCorrectColumns));
     }
 
-    private void calcRankingExpress(ArrayList<ArrayList<FieldCandidate>> correctColumns, int idealNumColumn) {
+    @SuppressWarnings("CallToThreadDumpStack")
+    private String getFieldExpression(String field) {
         try {
+            listObjectExtractors(); //>>>> Sérgio:  É melhor deixá-la aqui ou movê-la para o Singleton?? <<<<
 
-            HashMap<Integer, String> mapExpressions = new HashMap<>();
-            String betterExpression = null;
+            if (field != null) {
+                for (int j = 0; j < list.size(); j++) {
 
-            for (int i = 0; i < idealNumColumn; i++) {
-                for (int j = 0; j < correctColumns.size(); j++) {
+                    String expressionField;
+                    Pattern patNumVal = Pattern.compile(list.get(j).getExpression());
+                    Matcher matNumVal = patNumVal.matcher(field);
 
-                    String field = correctColumns.get(j).get(i).getField();
-                    String verReturn = getScoreField(field);
-
-                    if (!verReturn.isEmpty()) {
-                        System.out.print(" map: [" + j + "]"
-                                + mapExpressions.put(j, verReturn));
-                    }
-                    System.out.println("");
-                }
-            }
-
-            if (!mapExpressions.isEmpty()) {
-
-                Collection<String> valuesofMap;
-                valuesofMap = mapExpressions.values();
-                String high = Collections.max(valuesofMap);
-
-                for (Map.Entry<Integer, String> entry : mapExpressions.entrySet()) {
-
-                    if (high.equals(entry.getValue())) {
-
-                        betterExpression = entry.getValue();
+                    while (matNumVal.find()) {
+                        expressionField = list.get(j).getNameExpression();
+//                    System.out.println(field +" = " + expressionField +" ?");
+                        return expressionField;
                     }
                 }
-            }
-
-            System.out.println(betterExpression);
-
-        } catch (Exception error) {
-            /**
-             * Show the StackTrace error [for debug]
-             */
-            error.printStackTrace();
-//            System.out.println("Exception: " + error);
-        }
-    }
-
-    /**
-     *
-     * @param field
-     * @return
-     */
-    private String getScoreField(String field) {
-        try {
-            listObjectExtractors(); // É melhor deixá-la aqui ou movê-la para o Singleton??
-
-            for (int j = 0; j < list.size(); j++) {
-
-                String expField;
-                Pattern patNumVal = Pattern.compile(list.get(j).getExpression());
-                Matcher matNumVal = patNumVal.matcher(field);
-
-                while (matNumVal.find()) {
-                    expField = list.get(j).getNameExpression();
-                    return expField;
-                }
+            } else {
+                System.out.println("> ERROR: [In 'getFieldExpression' Method] >> The 'String field' parameter is Null!");
             }
         } catch (Exception error) {
             /**
@@ -233,19 +189,18 @@ public class AlignShortRecord {
         return null;
     }
 
+    @SuppressWarnings("CallToThreadDumpStack")
     private void listObjectExtractors() {
         try {
-            list = new ArrayList<>();
-
-            list.add(new CurrencyValuesExtractor());
-            list.add(new DateExtractor());
-            list.add(new EmailExtractor());
-            list.add(new PhoneFieldExtractor());
-            list.add(new TextExtractor());
-            list.add(new TimeExtractor());
-            list.add(new UrlsExtractor());
-            list.add(new ZipCodeBRExtractor());
-            list.add(new ZipCodeEUAExtractor());
+            list.add(new ZipCodeBRExtractor()); // 0.9
+            list.add(new EmailExtractor()); //0.85
+            list.add(new PhoneFieldExtractor()); // 0.8
+            list.add(new ZipCodeEUAExtractor()); // 0.75
+            list.add(new DateExtractor()); // 0.7
+            list.add(new TimeExtractor()); // 0.65
+            list.add(new UrlsExtractor()); // 0.6
+            list.add(new CurrencyValuesExtractor()); //0.2
+            list.add(new TextExtractor()); // 0.1
         } catch (Exception error) {
             /**
              * Show the StackTrace error [for debug]
@@ -255,18 +210,77 @@ public class AlignShortRecord {
         }
     }
 
-    private void printaMatriz(double[][] matrizScors) {
+    /**
+     *
+     * @param correctColumns
+     * @param idealNumColumn
+     * @param field
+     * @return
+     */
+    @SuppressWarnings("CallToThreadDumpStack")
+    private double scoreBasedExpressions(ArrayList<ArrayList<FieldCandidate>> correctColumns, int idealNumColumn, String field) {
+        try {
 
-        for (int j = 0; j < 4; j++) {
-            int t = 0;
-            for (int i = 0; i < 5; i++) {
+            String fieldExpression = null;
+            ArrayList<ArrayList<String>> allColumnExpressions = new ArrayList<ArrayList<String>>();
 
-                if (t <= 4) {
-                    System.out.print(matrizScors[j][i] + " | ");
-                    t++;
+            fieldExpression = getFieldExpression(field);
+            allColumnExpressions = expressionsInColumns(correctColumns, idealNumColumn);
+
+            if (!allColumnExpressions.isEmpty()) {
+                int occurrencesSum = 0;
+
+                for (int i = 0; i < idealNumColumn; i++) {
+                    for (int j = 0; j < allColumnExpressions.size(); j++) {
+                        occurrencesSum += funcaoCompara(fieldExpression, allColumnExpressions.get(i).get(j));
+                    }
                 }
+                double expressionScore = 0.0;
+                expressionScore = computePercentage(occurrencesSum, allColumnExpressions.size());
+
+                return expressionScore;
             }
-            System.out.println("");
+        } catch (Exception error) {
+            /**
+             * Show the StackTrace error [for debug].
+             */
+            error.printStackTrace();
+//            System.out.println(error);
         }
+        return Double.MAX_VALUE;
+    }
+
+    /**
+     *
+     * @param correctColumns
+     * @param idealNumColumn
+     * @return
+     */
+    @SuppressWarnings("CallToThreadDumpStack")
+    private ArrayList<ArrayList<String>> expressionsInColumns(ArrayList<ArrayList<FieldCandidate>> correctColumns, int idealNumColumn) {
+        try {
+
+            ArrayList<ArrayList<String>> allColumnExpressions = new ArrayList<ArrayList<String>>();
+
+            for (int i = 0; i < idealNumColumn; i++) {
+                ArrayList<String> columnExpressions = new ArrayList<String>();
+                for (int j = 0; j < correctColumns.size(); j++) {
+
+                    String fieldExpression = getFieldExpression(String.valueOf(correctColumns.get(j).get(i)));
+                    columnExpressions.add(fieldExpression);
+                }
+                allColumnExpressions.add(i, columnExpressions);
+            }
+
+            return allColumnExpressions;
+
+        } catch (Exception error) {
+            /**
+             * Show the StackTrace error [for debug]
+             */
+            error.printStackTrace();
+//            System.out.println(error);
+        }
+        return null;
     }
 }
